9af590f8235b6e9fd8796a8986e4eca263034b73
dg725 <davidgordon@DAVIDs-MacBook-Air-372.local>
2014-12-12 22:07:37 -0500

    Assignment #2
    Upload of assignment #2 completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,19 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## the inverse of the matrix in the cache if cacheSolve has been called
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        m <- NULL ##set m to NULL
+        set <- function(y) { ##modify existing matrix
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x ## return matrix input
+        setinverse <- function(inverse) m <<-inverse ##invert matrix values and set m globally
+        getinverse <- function() m ## return inverted matrix
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
